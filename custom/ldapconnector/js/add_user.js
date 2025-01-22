1c5da82f64603110907306ef1e03cdb5464fb15a
Quentin Marchand <114567503+QuentinEcollectivites@users.noreply.github.com>
2025-01-22 11:24:47 +0100

    Add files via upload

--- custom/ldapconnector/js/add_user.js
+++ custom/ldapconnector/js/add_user.js
@@ -1,6 +1,4 @@
 
-
-
 (function ($) {
   Drupal.behaviors.myModule = {
     attach: function (context, settings) {
@@ -9,35 +7,44 @@
         
         const nomInput = document.getElementById("edit-field-nom-0-value");
         const prenomInput = document.getElementById("edit-field-prenom-0-value");
-        const collectiviteInput = document.getElementById("edit-field-collectivite-0-target-id");
-        const username = document.getElementById("edit-field-collectivite-0-target-id");
-
-
+        const collectiviteSelect = document.getElementById("edit-field-collectivite");
+        const username = document.getElementById("edit-field-collectivite");
         updateNomUtilisateur();
         
         nomInput.addEventListener("input", updateNomUtilisateur);
         prenomInput.addEventListener("input", updateNomUtilisateur);
         username.addEventListener("input", updateNomUtilisateur);
         
-        if (collectiviteInput) {
-          collectiviteInput.addEventListener("input", updateNomUtilisateur); 
-          collectiviteInput.addEventListener("blur", updateNomUtilisateur);  // Lorsqu'il perd le focus
-          collectiviteInput.addEventListener("change", updateNomUtilisateur); // Lorsqu'une valeur est sélectionnée dans la liste
+        if (collectiviteSelect) {
+          collectiviteSelect.addEventListener("change", updateNomUtilisateur);
         }
       
         // Sélectionnez la case à cocher et le formulaire
       var checkboxPastell = $('#edit-field-groupes-applicatifs-67');
       var checkboxPastellActes = $('#edit-field-groupes-applicatifs-87');
-      var checkboxPastellConvoc = $('#edit-field-groupes-applicatifs-89');
-      var checkboxPastellDocs = $('#edit-field-groupes-applicatifs-84');
+      var checkboxPastellDocs = $('#edit-field-groupes-applicatifs-211');
       var checkboxPastellHelios = $('#edit-field-groupes-applicatifs-86');
-      var checkboxPastellChorus = $('#edit-field-groupes-applicatifs-91');
+	var checkboxPastellConvoc = $('#edit-field-groupes-applicatifs-89');
       var userDetailsWrapperPastell = $('#user-details-wrapper');
-
       var userDetailsActes = $('.form-item--user-details-0-field-role-agent-actes, .form-item--user-details-0-field-role-agent-actes-lecture')
       var userDetailsDocs = $('.form-item--user-details-0-field-role-agent-document')
       var userDetailsHelios = $('.form-item--user-details-0-field-role-agent-helios')
-      // Fonction pour afficher ou masquer le formulaire
+
+      function updatePastellCheckbox() {
+        if (checkboxPastellConvoc.is(':checked') || checkboxPastellActes.is(':checked') || checkboxPastellDocs.is(':checked') || checkboxPastellHelios.is(':checked') || checkboxPastellChorus.is(':checked')) {
+            checkboxPastell.prop('checked', true); // coche "Pastell"
+        } else {
+            checkboxPastell.prop('checked', false); // décoche "Pastell" si aucune autre n'est cochée
+        }
+    }
+    
+    // Associe la fonction à chaque case de "Actes", "Docs", "Helios" et "Chorus"
+    checkboxPastellConvoc.on('change', updatePastellCheckbox);
+    checkboxPastellActes.on('change', updatePastellCheckbox);
+    checkboxPastellDocs.on('change', updatePastellCheckbox);
+    checkboxPastellHelios.on('change', updatePastellCheckbox);
+    checkboxPastellChorus.on('change', updatePastellCheckbox);
+    checkboxPastell.on('change', updatePastellCheckbox);
       function toggleFormVisibilityPastell() {
         if (checkboxPastell.is(':checked')) {
           // Affichez le formulaire si la case à cocher est cochée
@@ -79,50 +86,39 @@
         }
       }
       
-      function updatePastellCheckbox() {
-        if (checkboxPastellConvoc.is(':checked') || checkboxPastellActes.is(':checked') || checkboxPastellDocs.is(':checked') || checkboxPastellHelios.is(':checked') || checkboxPastellChorus.is(':checked')) {
-            checkboxPastell.prop('checked', true); // coche "Pastell"
-        } else {
-            checkboxPastell.prop('checked', false); // décoche "Pastell" si aucune autre n'est cochée
-        }
-    }
-    
-    // Associe la fonction à chaque case de "Actes", "Docs", "Helios" et "Chorus"
-    checkboxPastellConvoc.on('change', updatePastellCheckbox);
-    checkboxPastellActes.on('change', updatePastellCheckbox);
-    checkboxPastellDocs.on('change', updatePastellCheckbox);
-    checkboxPastellHelios.on('change', updatePastellCheckbox);
-    checkboxPastellChorus.on('change', updatePastellCheckbox);
-
       // Vérifiez l'état de la case à cocher au chargement initial de la page
       toggleFormVisibilityPastell()
       toggleFormVisibilityPastellRole()
       toggleFormVisibilityPastellDocs()
       toggleFormVisibilityPastellHelios()
-
       // Ajoutez un gestionnaire d'événements pour la case à cocher
       checkboxPastell.on('change', toggleFormVisibilityPastell);
       checkboxPastellActes.on('change', toggleFormVisibilityPastellRole);
       checkboxPastellDocs.on('change',  toggleFormVisibilityPastellDocs);
       checkboxPastellHelios.on('change',  toggleFormVisibilityPastellHelios);
-
-function updateNomUtilisateur() {
-          let partieCollectivite = '';
-          if (collectiviteInput) {
-            const selectedText = collectiviteInput.value.split(" (")[0]; // Utilisation de la valeur entrée par l'utilisateur
+      function updateNomUtilisateur() {
+          let partieCollectivite;
+      
+          // Get the selected value
+          if (collectiviteSelect) {
+            const selectedOption = collectiviteSelect.options[collectiviteSelect.selectedIndex];
+            const selectedText = selectedOption.text;
             partieCollectivite = removeSpecialCharacters(selectedText.toLowerCase());
+          } else {
+            const nomUtilisateurInput = document.getElementById("edit-name").value;
+            const parts = nomUtilisateurInput.split('@');
+            partieCollectivite = parts[1];
           }
-
           const nom = removeSpecialCharacters(nomInput.value);
           const prenom = removeSpecialCharacters(prenomInput.value);
+          
           const partiePrenomNom = `${prenom}.${nom}`.toLowerCase();
-
           document.getElementById("edit-field-nom-0-value").value = nom;
           document.getElementById("edit-field-prenom-0-value").value = prenom;
           document.getElementById("edit-name").value = `${partiePrenomNom}@${partieCollectivite}`;
-document.getElementById("edit-cas-username").value = `${partiePrenomNom}@${partieCollectivite}`;
+          document.getElementById("edit-cas-username").value = `${partiePrenomNom}@${partieCollectivite}`;
         }
-
+      
         // Fonction pour supprimer les caractères spéciaux
         function removeSpecialCharacters(text) {
           return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^a-zA-Z0-9]/g, "");
